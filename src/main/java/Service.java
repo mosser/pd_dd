83c3eb16cd31f44ccfdff300f923326f9729be03
Sébastien Mosser <mosser.sebastien@uqam.ca>
2021-10-07 10:33:16 -0400

    first commit

---
+++ src/main/java/Service.java
@@ -0,0 +1,46 @@
+import io.jooby.Jooby;
+import io.jooby.ServerOptions;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Service extends Jooby {
+
+    public static void main(String[] args) { runApp(args, Service::new); }
+
+    {
+        // Added to control the deployment port (Heroku deployment, Step V.2)
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+        setServerOptions(new ServerOptions().setPort(port));
+
+        get("/", ctx -> "Welcome to our drink ordering system");
+        get("/orders", ctx -> getAllOrders());
+        get("/orders/{owner}/{recipient}/{drink}", ctx -> {
+            Order o = addOrder(ctx.path("owner").value(),
+                               ctx.path("recipient").value(),
+                               ctx.path("drink").value());
+            return "added " + o;
+        });
+    }
+
+    private final List<Order> orders = new LinkedList<>();
+
+    public String getAllOrders() {
+        if(orders.isEmpty())
+            return "Nothing to show";
+        return orders.stream()
+                .map(Order::toString)
+                .reduce("",(s1,s2) -> s1 +"\n" + s2);
+    }
+
+    public Order addOrder(String owner, String recipient, String drinkName) {
+        Order o = new Order();
+        o.setOwner(owner);
+        o.setRecipient(recipient);
+        o.getDrinks().add(new Order.Drink(drinkName));
+        orders.add(o);
+        return o;
+    }
+
+
+}
